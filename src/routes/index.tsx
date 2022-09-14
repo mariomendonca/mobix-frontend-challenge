45c03b55648e28c60d8eef0c91000ffe98d8652e
mario mendonca <mariomeendonca00@gmail.com>
2022-09-13 23:27:44 -0300

    first commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native"
+
+export function Routes() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' }}>
+      <Text>Hello world aaaaa</Text>
+    </View>
+  )
+}
